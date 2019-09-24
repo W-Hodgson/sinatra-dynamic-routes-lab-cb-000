453088e94804f5024d1a8cb3dfdc36558f02c1a6
Will Hodgson <will.hodgson@current-rms.com>
2019-09-24 13:09:16 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -38,7 +38,7 @@ class App < Sinatra::Base
     @number2 = params[:number2].to_i
 
     if @operation == "add"
-      "#{@number1}"
+      "#{@number1+@number2}"
     elsif @operation == "multiply"
 
     elsif @operation == "subtract"
